851a3783d65a25f41d01d478757b448ceff8fccd
Roger Rezende <javielrezende@gmail.com>
2021-09-13 22:53:35 -0300

    :sparkles: Creates adapter to database
    Creates adapter to database

---
+++ adapters/db/product.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/javielrezende/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+
+	stmt, err := p.db.Prepare("select id, name , price, status from products where id=?")
+
+	if err != nil {
+		return nil, err
+	}
+
+	// O scan pega os valores e adiciona nos atributos da classe
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/golang/mock v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.8
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.7.0
 )
